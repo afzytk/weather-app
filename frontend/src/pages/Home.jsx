10571d966256f3c8e39c7e137d44e952ffc26fba
afzytk <afzytk@gmail.com>
2025-11-29 16:26:20 +0530

    feat: add structured Home page layout with semantic sections

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from ".../components/Navbar";
+
+const Home = () => {
+  return (
+  <>
+  <Navbar />
+  <main className="max-w-3xl mx-auto p-6">
+
+  </main>
+
+  <section className="max-w-3xl mx-auto p-6 mt-12"></section>
+  </>
+};
